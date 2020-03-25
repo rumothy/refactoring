2e47e58515c60c92785b14c9f7535556af92e815
rumothy <rumothy@yahoo.com>
2020-03-25 19:22:36 -0400

    The Starting Point

--- 01-AFirstExample/main.js
+++ 01-AFirstExample/main.js
@@ -1,10 +1,33 @@
-// import plays from "./plays.json";
-// import invoices from "./invoices.json";
+const plays = {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" }
+};
+
+const invoices = [
+  {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55
+      },
+      {
+        playID: "as-like",
+        audience: 35
+      },
+      {
+        playID: "othello",
+        audience: 40
+      }
+    ]
+  }
+];
 
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.coustomer}\n`;
+  let result = `Statement for ${invoice.customer}\n`;
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -12,7 +35,8 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    switch (plays.type) {
+    const play = plays[perf.playID];
+    switch (play.type) {
       case "tragedy":
         thisAmount = 40000;
         if (perf.audience > 30) {
@@ -20,9 +44,28 @@ function statement(invoice, plays) {
         }
         break;
       case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
         break;
       default:
         throw new Error(`uknown type: ${play.type}`);
     }
+
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    result += `   ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    } seats)\n`;
+    totalAmount += thisAmount;
   }
+
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
 }
+
+console.log(statement(invoices[0], plays));
