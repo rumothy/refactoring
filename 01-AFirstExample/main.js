c9ac888117b8b657430c9f79b18be15b22ef9372
rumothy <rumothy@yahoo.com>
2020-03-30 05:39:22 -0400

    add text

--- 01-AFirstExample/main.js
+++ 01-AFirstExample/main.js
@@ -1,28 +1,30 @@
-const plays = {
-  hamlet: { name: "Hamlet", type: "tragedy" },
-  "as-like": { name: "As You Like It", type: "comedy" },
-  othello: { name: "Othello", type: "tragedy" }
-};
+// const plays = {
+//   hamlet: { name: "Hamlet", type: "tragedy" },
+//   "as-like": { name: "As You Like It", type: "comedy" },
+//   othello: { name: "Othello", type: "tragedy" }
+// };
 
-const invoices = [
-  {
-    customer: "BigCo",
-    performances: [
-      {
-        playID: "hamlet",
-        audience: 55
-      },
-      {
-        playID: "as-like",
-        audience: 35
-      },
-      {
-        playID: "othello",
-        audience: 40
-      }
-    ]
-  }
-];
+// const invoices = [
+//   {
+//     customer: "BigCo",
+//     performances: [
+//       {
+//         playID: "hamlet",
+//         audience: 55
+//       },
+//       {
+//         playID: "as-like",
+//         audience: 35
+//       },
+//       {
+//         playID: "othello",
+//         audience: 40
+//       }
+//     ]
+//   }
+// ];
+
+import plays from "./plays.json";
 
 function statement(invoice, plays) {
   let totalAmount = 0;
